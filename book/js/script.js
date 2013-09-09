0a87b7c7f44cbe97855d6237a0351b480a9aca80
Rachel Nabors <rachelnabors@gmail.com>
2013-09-09 15:51:32 -0400

    Improvingthe tunnel experience: better SVG and turning off borked skrollr mobile experience.

--- book/js/script.js
+++ book/js/script.js
@@ -1,129 +1,106 @@
-$(window).load(function() {
 /* Alice in Videoland JS! */
 
-var $hole = $("#to-tunnels"),
-		$tunnels = $("#tunnels"),
+$(window).load(function() {
+
+var $tunnels = $("#tunnels"),
 		$tunnel = $("#tunnel");
 
-// Reveal the rabbit tunnel
-var downTheHole = function() {
+	// Rodney Rehm's improved method for mapping click and touch: https://gist.github.com/rodneyrehm/6464641
+  function touch(event) {
+      event.preventDefault();
+      var runFunc = $(this).data('activateRunFunc');
+      runFunc() && runFunc();;
+  }
+  
+  function click(event) {
+      event.preventDefault();
+      event.stopImmediatePropagation();
+      var runFunc = $(this).data('activateRunFunc');
+      runFunc() && runFunc();;
+  }
+  
+  $.fn.activate = function(runFunc) {
+      return $(this).data('activateRunFunc', runFunc).on({
+          touchend: touch,
+          click: click
+      });
+  };
 
-	// reveal the tunnels w/ class
-	$tunnels.addClass("cue");
 
-	// recalculate the new waypoints since this part was hidden
-	$('.page').waypoint('disable');
-	$.waypoints('refresh'); //refreshing recalculates where the waypoints are, but it also fires all their callbacks! 
-	$('.page').waypoint('enable');
+	// Reveal the rabbit tunnel and move the page down to #tunnel
+	var downTheHole = function() {
 
-	$tunnel.addClass("in-view");
+		var $alice = $tunnel.find(".alice-falling");
 
-}
-
-// How to guess which frame is "being read".
-var beingRead = function() {
-	// It would be approximately centered, of equal distance from top as from bottom.	
-	var $screenHeight = $.waypoints('viewportHeight');
-	var $pageHeight = $(".page").height();
-	var offset = ($pageHeight - $screenHeight) / 2 * -1;
-
-	return offset;
-}
-
-var gentleScroll = function(duration, scrollPast) {
-		// gently animate down the page
-		var scrolledHeight = $(window).scrollTop(); // current offset from top
-		var windowHeight = $(window).height(); //window's height
-		if (scrollPast) {
-			var scrollPastHeight = $(scrollPast).outerHeight(); // all "pages" are the same height...
-			var newOffset = scrolledHeight + scrollPastHeight + windowHeight; 
-		} else {
-			var newOffset = scrolledHeight + windowHeight; 			
-		}
-		$("html,body").animate({
-			scrollTop: newOffset // animate new offset to scroll past the tunnels
-		}, duration, function(){
-			//make sure it's set to the correct body class.
-			var mood = $tunnel.find(".page").first().data("mood");
-			$("body").removeClass().addClass(mood);
-		});
-}
-
-// For people touching the screen
-$hole.on('touchend', function(e){
-	$(this).addClass("touched");
-	
-	$(".alice-falling").addClass("touched");
-
-	downTheHole();
-
-	gentleScroll(20000, $tunnels);
-
-	e.preventDefault();
-});
-
-// For people using mice
-$hole.on('click', function(e){
-	if (!($(this).hasClass('touched'))) {
-		//make Alice sticky
-		$(".alice-falling").waypoint('sticky');
-
-		downTheHole();
+		// reveal the tunnels w/ class
+		$tunnels.addClass("cue");
 
 		// calculate the height of the tunnels
 		var tunnelTop = Math.round($tunnel.offset().top);
 		var tunnelTopData = "data-" + tunnelTop;
 		var tunnelBottomData =  "data-" + (tunnelTop + Math.round($tunnel.height()));
 
-		// Give Falling Alice her skrollr measurements as data attributes
-		$tunnel.find(".alice-falling").attr(tunnelTopData, "top:0%").attr(tunnelBottomData, "top:80%");
+		$alice.waypoint('sticky') // make her sticky!
+		.attr(tunnelTopData, "top:0%").attr(tunnelBottomData, "top:80%");		// Give Falling Alice her skrollr measurements as data attributes
+
+
+		// recalculate the new waypoints since this part was hidden
+		$.waypoints('refresh');
+
+		// Give the parent tunnel an in-view class
+		$tunnel.addClass("in-view");
+
+		// gently animate down the page
+		var scrolledHeight = $(window).scrollTop(); // current offset from top
+		var windowHeight = $(window).height(); //window's height
+		var tunnelsHeight = $(".page_tunnels").outerHeight(); // all "pages" are the same height...
+		var newOffset = scrolledHeight + tunnelsHeight + windowHeight; 
+		$("html,body").animate({
+			scrollTop: newOffset // animate new offset to scroll past the tunnels
+		}, 4000, function(){
 		
-		//Start up the skrollr object
-		skrollr.init({
-			forceHeight: false
+			//Start up the skrollr object
+			skrollr.init({
+				forceHeight: false,
+				mobileCheck: function() {
+              //hack - forces mobile version to be off
+              return false;
+          }
+			});
 		});
-
-		gentleScroll(2000);
-		
-		e.preventDefault();
-		e.stopPropagation();
-	};
-});
-
-// play phone SFX when the rabbit appears in full view. 
-$(".page_rabbit-appears").waypoint(function() {
-	if ($(this).hasClass("in-view")){
-		var phone = document.getElementById("rabbit-phone");
-		phone.volume = 0.1;
-		setTimeout(function(){
-			phone.play();
-		}, 2000);
 	}
-}, {
-  offset: beingRead()
-});
 
+	// How to guess which frame is "being read".
+	var beingRead = function() {
+		// It would be approximately centered, of equal distance from top as from bottom.	
+		var $screenHeight = $.waypoints('viewportHeight');
+		var $pageHeight = $(".page").height();
+		var offset = ($pageHeight - $screenHeight) / 2 * -1;
 
-// all pages get in-view classes while they're centered in the viewport 
-$(".page").waypoint(function(direction) {
-	$(this).addClass("in-view");
-
-	// Change attitude as you "fall"
-	if ($(this).parent("#tunnel")){
-		var mood = $(this).data("mood");
-		$("body").removeClass().addClass(mood);		
+		return offset;
 	}
-}, {
-  offset: beingRead() //makes sure the frame is centered before adding class
-});
 
+	// To get to #tunnels, activate #tunnels
+	$("#to-tunnels").activate(downTheHole);
 
-	// Have Tuna float up
+	// play phone SFX when the rabbit appears in full view. 
+	$(".page_rabbit-appears").waypoint(function() {
+		if ($(this).hasClass("in-view")){
+			var phone = document.getElementById("rabbit-phone");
+			phone.volume = 0.1;
+			setTimeout(function(){
+				phone.play();
+			}, 2000);
+		}
+	});
 
-	// Add Tuna craziness
+	// all pages get in-view classes while they're centered in the viewport 
+	$(".page").waypoint(function() {
+		$(this).addClass("in-view");
+	}, {
+	  offset: beingRead()
+	});
 
-	// Have Tuna float away
-
-	// Once you get to the end of the tunnels, cue cut and wonderland behind it
-	// $(".scene-cut, .scene-wonderland").addClass("cue");
+	// After cut, show wonderland
+	// Roll credits
 });
