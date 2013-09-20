aea63de9c00c4a95c72ac1bdde4716a1132ac8f1
Rachel Nabors <rachelnabors@gmail.com>
2013-09-20 11:12:34 +0200

    Fixing the final scene's Alice and Rabbit.

--- book/js/script.js
+++ book/js/script.js
@@ -29,120 +29,126 @@
 	    });
 	};
 
-		// if the page's height is greater than the window's height, just scroll to its top edge
-		var topOffset = function(page) {
-			var $topCorner = $(page).offset().top,
-					diff = ($(page).outerHeight() - $screenHeight) / 2;
-			if(diff >= 0) { 
-				return $topCorner;
-			} else {
-				return $topCorner + diff;
-			}
+	// if the page's height is greater than the window's height, just scroll to its top edge
+	var topOffset = function(page) {
+		var $topCorner = $(page).offset().top,
+				diff = ($(page).outerHeight() - $screenHeight) / 2;
+		if(diff >= 0) { 
+			return $topCorner;
+		} else {
+			return $topCorner + diff;
 		}
+	}
 
-		var scrollPageIntoCenter = function(toPage) {
-			// Then animate that sucker.
-			$("html, body").animate({
-					scrollTop: topOffset(toPage)
-				}, 500);
-		};
+	var scrollPageIntoCenter = function(toPage) {
+		// Then animate that sucker.
+		$("html, body").animate({
+				scrollTop: topOffset(toPage)
+			}, 500);
+	};
 
-		// if touch is enabled, let's use hammer.js to detect gestures!
-		if(Modernizr.touch) {
-			//prevent scrolling 
-			// ev.gesture.preventDefault();
-			$(document).hammer({drag_block_vertical: true}).on("swipeup", ".page", function(event) {
-				// alert("swiped!")
-				var $nextPage = $(this).next(".page");
-				scrollPageIntoCenter($nextPage);
-			});
+	// if touch is enabled, let's use hammer.js to detect gestures!
+	if(Modernizr.touch) {
+		//prevent scrolling 
+		// ev.gesture.preventDefault();
+		$(document).hammer({drag_block_vertical: true}).on("swipeup", ".page", function(event) {
+			// alert("swiped!")
+			var $nextPage = $(this).next(".page");
+			scrollPageIntoCenter($nextPage);
+		});
 
-			//Swipe to previous page
-			$(document).hammer({drag_block_vertical: true}).on("swipedown", ".page", function(event) {
-				var $prevPage = $(this).prev(".page");
-				scrollPageIntoCenter($prevPage);
-			});
+		//Swipe to previous page
+		$(document).hammer({drag_block_vertical: true}).on("swipedown", ".page", function(event) {
+			var $prevPage = $(this).prev(".page");
+			scrollPageIntoCenter($prevPage);
+		});
 
-		}
+	}
 
-	  setTimeout(function(){ 
-	    // change state to loaded
-	    $("html").addClass("loaded").removeClass("loading");
-	    
-	    // Show park scene.
-	    $(".scene-park").addClass("cue");
-	    
-	    // all pages get in-view classes while they're centered in the viewport 
-			$(".scene-park .page").waypoint(function() {
-				$(this).addClass("in-view");
-			}, {
-			  offset: beingRead()
-			});
-	  }, 5000); //gotta wait a lil' bit
+	setTimeout(function(){ 
+		// change state to loaded
+		$("html").addClass("loaded").removeClass("loading");
 
-		// Reveal the rabbit tunnel and move the page down to #tunnel
-		var downTheHole = function() {
+		// Show park scene.
+		$(".scene-park").addClass("cue");
 
-			var $alice = $tunnel.find(".alice-falling");
+		// all pages get in-view classes while they're centered in the viewport 
+		$(".scene-park .page").waypoint(function() {
+			$(this).addClass("in-view");
+		}, {
+		  offset: beingRead()
+		});
+	}, 5000); //gotta wait a lil' bit
 
-			// reveal the tunnels w/ class
-			$tunnels.addClass("cue");
+	// Reveal the rabbit tunnel and move the page down to #tunnel
+	var downTheHole = function() {
 
-			// calculate the height of the tunnels
-			var tunnelTop = Math.round($tunnel.offset().top);
-			var tunnelTopData = "data-" + tunnelTop;
-			var tunnelBottomData =  "data-" + (tunnelTop + Math.round($tunnel.height()));
+		var $alice = $tunnel.find(".alice-falling");
 
-			$alice.waypoint('sticky') // make her sticky!
-			.attr(tunnelTopData, "top:0%").attr(tunnelBottomData, "top:80%");		// Give Falling Alice her skrollr measurements as data attributes
+		// reveal the tunnels w/ class
+		$tunnels.addClass("cue");
+
+		// calculate the height of the tunnels
+		var tunnelTop = Math.round($tunnel.offset().top);
+		var tunnelTopData = "data-" + tunnelTop;
+		var tunnelBottomData =  "data-" + (tunnelTop + Math.round($tunnel.height()));
+
+		$alice.waypoint('sticky') // make her sticky!
+		.attr(tunnelTopData, "top:0%").attr(tunnelBottomData, "top:80%");		// Give Falling Alice her skrollr measurements as data attributes
 
 
-			// recalculate the new waypoints since this part was hidden
-			$tunnel.find(".page").waypoint(function() {
-				$(this).addClass("in-view");
-				var mood = $(this).data("mood");
-				$("body").removeClass().addClass(mood);		
-			}, {
-			  offset: beingRead()
-			});
+		// recalculate the new waypoints since this part was hidden
+		$tunnel.find(".page").waypoint(function() {
+			$(this).addClass("in-view");
+			var mood = $(this).data("mood");
+			$("body").removeClass().addClass(mood);		
+		}, {
+		  offset: beingRead()
+		});
 
-			// Give the parent tunnel an in-view class
-			$tunnel.addClass("in-view")
-			.find(".page:first").addClass("in-view");// and give it's first page an in-view class ;)
+		// Give the parent tunnel an in-view class
+		$tunnel.addClass("in-view")
+		.find(".page:first").addClass("in-view");// and give it's first page an in-view class ;)
 
-			// gently animate down the page
-			$("html,body").animate({
-				scrollTop: $("#tunnel").offset().top // animate new offset to scroll past the tunnels
-			}, 4000, function(){
-				$(".scene-hole").waypoint(function(direction) {
-					// Disable waypoint so people can scroll up if they want to.
-					$(".scene-hole").waypoint('disable');
+		// gently animate down the page
+		$("html,body").animate({
+			scrollTop: $("#tunnel").offset().top // animate new offset to scroll past the tunnels
+		}, 4000, function(){
+			$(".scene-hole").waypoint(function(direction) {
+				// Disable waypoint so people can scroll up if they want to.
+				$(".scene-hole").waypoint('disable');
 
-					// gently animate down the page
-			  	$(this).next(".scene").addClass("cue");
-			  	window.setTimeout(function(){
-			  		$(".scene-wonderland").addClass("cue");
-				  	$("html,body").animate({
-							scrollTop: $(".scene-wonderland").offset().top
-						}, 2500, function() {
-							$(".scene-cut").remove();
-						});
-			  	}, 2000);
+				// gently animate down the page
+		  	$(this).next(".scene").addClass("cue");
+		  	window.setTimeout(function(){
+		  		$(".scene-wonderland").addClass("cue")
+		  		.find(".page").waypoint(function() {
+					$(this).addClass("in-view");
 				}, {
-				  offset: function() {
-				    return -($tunnels.height() - $screenHeight);
-				  }
+				  offset: beingRead()
 				});
-				//Start up the skrollr object
-				skrollr.init({
-					forceHeight: false,
-					mobileCheck: function() {
-	              //hack - forces mobile version to be off
-	              return false;
-	          }
-				});
+
+			  	$("html,body").animate({
+						scrollTop: $(".scene-wonderland").offset().top
+					}, 2500, function() {
+						$(".scene-cut").remove();
+					});
+		  	}, 2000);
+			}, {
+			  offset: function() {
+			    return -($tunnels.height() - $screenHeight);
+			  }
 			});
-		}
+			//Start up the skrollr object
+			skrollr.init({
+				forceHeight: false,
+				mobileCheck: function() {
+              //hack - forces mobile version to be off
+              return false;
+          }
+			});
+		});
+	}
 
 	// How to guess which frame is "being read".
 	var beingRead = function() {
