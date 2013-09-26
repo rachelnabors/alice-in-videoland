a564a0cf70a625a6882fad6e1250068adcd16c00
Rachel Nabors <rachelnabors@gmail.com>
2013-09-26 18:25:13 -0400

    improving loader's font sizing and such.

--- book/js/script.js
+++ book/js/script.js
@@ -13,7 +13,7 @@
   window.addEventListener('orientationchange', recalcWaypoints);
 
 	// First and foremost, get that loader in place.
-	var loadingSaucer= '<div id="loader"><svg version="1.1" id="brew" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="266px" height="25px" viewBox="0 0 266 25" enable-background="new 0 0 266 25" xml:space="preserve"><ellipse fill="#903741" cx="133" cy="12.5" rx="133" ry="12.5"/></svg><p>Please wait while we load</p></div><div class="saucer"></div>';
+	var loadingSaucer= '<div id="loader"><div class="cup"><svg version="1.1" id="brew" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="266px" height="25px" viewBox="0 0 266 25" enable-background="new 0 0 266 25" xml:space="preserve"><ellipse fill="#903741" cx="133" cy="12.5" rx="133" ry="12.5"/></svg><p>Please wait while we load</p></div><div class="saucer"></div></div>';
 	$(".loading-card").append(loadingSaucer);
 
 	// Rodney Rehm's improved method for mapping click and touch: https://gist.github.com/rodneyrehm/6464641
