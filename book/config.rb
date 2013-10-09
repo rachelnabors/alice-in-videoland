90d62e8c402209300bdb5ac9b1c8a1b22d880a9b
Rachel Nabors <rachelnabors@gmail.com>
2013-10-09 13:00:56 -0400

    Adding Singularity grids

--- book/config.rb
+++ book/config.rb
@@ -1,6 +1,8 @@
 # Require any additional compass plugins here.
 require 'animation-studio'
 require 'breakpoint'
+require 'singularitygs'
+require 'singularity-extras'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
