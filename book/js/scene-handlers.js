7c1ffc0216bd566e58bd828d92c29fd9608be78d
Rachel Lee Nabors <rachelnabors@gmail.com>
2024-06-04 14:59:40 +0100

    add spring to crash with linear easing

--- book/js/scene-handlers.js
+++ book/js/scene-handlers.js
@@ -1,4 +1,4 @@
-import { jumpToAnchor } from "./utilities.js";
+import { jumpToAnchor, isReduced } from "./utilities.js";
 
 const cutScene = document.getElementById("scene-cut");
 // Each SCENE gets a class of CUE when it's XX in the VIEWPORT?;
@@ -32,7 +32,7 @@ const pagesObserver = new IntersectionObserver(
       }
     });
   },
-  { threshold: 0.75 },
+  { threshold: 0.5 },
 );
 
 export let pageObserverHandler = (page) => {
@@ -43,10 +43,31 @@ export let pageObserverHandler = (page) => {
 const tunnelEndObserver = new IntersectionObserver(
   (page) => {
     const sceneWonderland = document.getElementById("scene_wonderland");
-
+    const jiggle = () => {
+      if (!isReduced) {
+        // jiggle the page
+        document.querySelector(".page_crash-landing").animate(
+          [
+            { transform: "scale(.8) rotate(-10deg) translateY(50%)" },
+            {
+              transform: "scale(1.2) rotate(10deg) translateY(-50%)",
+              offest: 0.5,
+            },
+            { transform: "scale(1) rotate(0) translateY(0)" },
+          ],
+          {
+            duration: 2000,
+            fill: "forwards",
+            easing:
+              "linear(0, 0.218 2.1%, 0.862 6.5%, 1.114, 1.296 10.7%, 1.346, 1.37 12.9%, 1.373,1.364 14.5%, 1.315 16.2%, 1.032 21.8%, 0.941 24%, 0.891 25.9%, 0.877,0.869 27.8%, 0.87, 0.882 30.7%, 0.907 32.4%, 0.981 36.4%, 1.012 38.3%, 1.036, 1.046 42.7% 44.1%, 1.042 45.7%, 0.996 53.3%, 0.988, 0.984 57.5%, 0.985 60.7%,1.001 68.1%, 1.006 72.2%, 0.998 86.7%, 1)",
+          },
+        );
+      }
+    };
     if (page[0].isIntersecting) {
       // At end of page, fade to black (on animationend)
-      cut(sceneWonderland);
+      cut(sceneWonderland, jiggle);
+
       tunnelEndObserver.unobserve(page[0].target);
     }
   },
@@ -57,7 +78,7 @@ export let tunnelEndObserverHandler = (page) => {
   tunnelEndObserver.observe(page);
 };
 
-const cut = (nextScene) => {
+const cut = (nextScene, postCutFunc) => {
   const fadeToBlackKeyframes = new KeyframeEffect(
     cutScene,
     [{ opacity: 0 }, { opacity: 1 }],
@@ -81,6 +102,7 @@ const cut = (nextScene) => {
         document.timeline,
       );
       fadeInFromBlack.play();
+      postCutFunc();
     };
     document.addEventListener("scroll", endScroll, { once: true });
   };
